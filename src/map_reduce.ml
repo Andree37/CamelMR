9df9b737a74228696f8f05a6ce61d9ac0460612e
Andre Ribeiro <andresmribeiro@gmail.com>
2023-09-28 21:26:59 +0100

    feat: improved mapreduce

--- src/map_reduce.ml
+++ src/map_reduce.ml
@@ -1,58 +1,47 @@
 let find_column_index headers column_name =
-  try 
-      let indexed_headers = List.mapi (fun i col -> (i, col)) headers in
-      let (index, _) = List.find (fun (_, col) -> col = column_name) indexed_headers in
-      Some index
-  with Not_found -> None
+  let rec aux headers idx =
+    match headers with
+    | [] -> None
+    | hd :: _ when hd = column_name -> Some idx
+    | _ :: tl -> aux tl (idx + 1)
+  in
+  aux headers 0
 
-let create_map_function target_index = 
-  fun line ->
-      let ty = List.nth line target_index in 
-      (ty, 1)
-  
-let group_by_key kv_pairs = 
+
+
+let create_map_function target_index line =
+  (List.nth line target_index, 1)
+
+let add_to_hashtbl table (k, v) =
+  try
+    let existing = Hashtbl.find table k in
+    Hashtbl.replace table k (existing + v)
+  with Not_found ->
+    Hashtbl.add table k v
+
+let parallel_map input_file map_function =
+  let ic = open_in input_file in
+  let csv = Csv.of_channel ic in
   let table = Hashtbl.create 100 in
-  List.iter (fun (k, v) ->
-      let existing_values = try Hashtbl.find table k with Not_found -> [] in
-      Hashtbl.replace table k (v :: existing_values)
-  ) kv_pairs;
+  
+  Csv.iter ~f:(fun row ->
+    add_to_hashtbl table (map_function row)
+  ) csv;
+  
   table
-
-
-let reduce_group key values = 
-  key, (List.fold_left (+) 0 values)
-
-let parallel_reduce table = 
-  let grouped = Hashtbl.fold (fun k v acc -> (k, v)::acc) table [] in
-  Parmap.parmap ~ncores:4 (fun (k, vs) -> reduce_group k vs) (Parmap.L grouped)
-
 
 let compare_by_value (_, v1) (_, v2) = 
   v2 - v1
   
-  let map_reduce input_file target_column_name =
-    let ic = open_in input_file in
-    let csv = Csv.of_channel ic in
-  
-    let headers = Csv.next csv in
-    match find_column_index headers target_column_name with
-    | None -> 
-        Printf.printf "Column name \"%s\" not found in CSV.\n" target_column_name;
-        exit 1;
-    | Some idx ->
-        let map_function = create_map_function idx in
-        let mapped_values_ref = ref [] in
-        let lines_read = ref 0 in
-        
-        Csv.iter ~f:(fun row ->
-            mapped_values_ref := (map_function row) :: !mapped_values_ref;
-            incr lines_read; 
-        ) csv;
-  
-        let grouped = group_by_key !mapped_values_ref in
-        let reduced: (string * int) list = parallel_reduce grouped in
-  
-        let sorted_results = List.sort compare_by_value reduced in
-        List.iter (fun (k, v) -> Printf.printf "Type: %s, Sum: %d\n" k v) sorted_results;
-        Printf.printf "Read %d lines\n" !lines_read;
-  
+let map_reduce input_file target_column_name =
+  let headers = Csv.next (Csv.of_channel (open_in input_file)) in
+  match find_column_index headers target_column_name with
+  | None -> 
+    Printf.printf "Column name \"%s\" not found in CSV.\n" target_column_name;
+    exit 1;
+  | Some idx ->
+    let mapped_table = parallel_map input_file (create_map_function idx) in
+    let reduced = Hashtbl.fold (fun k v acc -> (k, v) :: acc) mapped_table [] in
+    let sorted_results = List.sort compare_by_value reduced in
+    List.iter (fun (k, v) -> Printf.printf "Type: %s, Sum: %d\n" k v) sorted_results;
+    Printf.printf "Read %d lines\n" (Hashtbl.length mapped_table);
