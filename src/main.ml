d1fb55ec3767d3dd647fbd886df4a34038977520
Andre Ribeiro <andresmribeiro@gmail.com>
2023-09-28 14:27:36 +0100

    feat: parallel

--- src/main.ml
+++ src/main.ml
@@ -12,6 +12,7 @@ let () =
         let start_time = Unix.gettimeofday () in
         let _ = match mode with
             | "simple" -> Simple_reduce.simple_reduce input_file target_column_name
+            | "parallel" -> Parallel_reduce.parallel_reduce input_file target_column_name 20000 (* this value depends on the available ram *)
             | _ -> Map_reduce.map_reduce input_file target_column_name
         in
 
--- src/parallel_reduce.ml
+++ src/parallel_reduce.ml
@@ -5,50 +5,71 @@ let find_column_index headers column_name =
         Some index
     with Not_found -> None
 
-let parallel_simple_reduce input_file target_column_name =
-  let ic = open_in input_file in
-  let csv = Csv.of_channel ic in
 
-  let headers = Csv.next csv in
-  match find_column_index headers target_column_name with
-  | None -> 
-      Printf.printf "Column name \"%s\" not found in CSV.\n" target_column_name;
-      exit 1;
-  | Some idx ->  
-      let count_partition row_list = 
-          let local_table = Hashtbl.create 100 in
-          List.iter (fun row ->
-              let column_value = List.nth row idx in
-              let count = Hashtbl.find_opt local_table column_value in
-              match count with
-              | Some n -> Hashtbl.replace local_table column_value (n+1)
-              | None -> Hashtbl.add local_table column_value 1;
-          ) row_list;
-          local_table
-      in
+let merge_tables tables =
+    let merged = Hashtbl.create 100 in
+    List.iter (fun table ->
+        Hashtbl.iter (fun k v ->
+            let current = Hashtbl.find_opt merged k in
+            match current with
+            | Some n -> Hashtbl.replace merged k (n+v)
+            | None -> Hashtbl.add merged k v;
+        ) table;
+    ) tables;
+    merged
 
-      let merge_tables tables =
-          let merged = Hashtbl.create 100 in
-          List.iter (fun table ->
-              Hashtbl.iter (fun k v ->
-                  let current = Hashtbl.find_opt merged k in
-                  match current with
-                  | Some n -> Hashtbl.replace merged k (n+v)
-                  | None -> Hashtbl.add merged k v;
-              ) table;
-          ) tables;
-          merged
-      in
+let parallel_reduce input_file target_column_name batch_size =
+    let ic = open_in input_file in
+    let csv = Csv.of_channel ic in
 
-      let csv_list = Csv.input_all csv in
-      let partitions = Parmap.parmap ~ncores:4 count_partition (Parmap.L csv_list) in
-      let merged_results = merge_tables partitions in
+    let headers = Csv.next csv in
+    match find_column_index headers target_column_name with
+    | None -> 
+        Printf.printf "Column name \"%s\" not found in CSV.\n" target_column_name;
+        exit 1;
+    | Some idx ->  
+        let lines_read = ref 0 in
+        let batch = ref [] in
+        let all_results = ref [] in
 
-      let sorted_results = 
-          Hashtbl.to_seq merged_results 
-          |> List.of_seq
-          |> List.sort (fun (_, v1) (_, v2) -> v2 - v1) 
-      in
+        let process_batch batch =
+            let count_table = Hashtbl.create 100 in
+            List.iter (fun row ->
+                let column_value = List.nth row idx in
+                let count = Hashtbl.find_opt count_table column_value in
+                match count with
+                | Some n -> Hashtbl.replace count_table column_value (n+1)
+                | None -> Hashtbl.add count_table column_value 1;
+            ) batch;
+            count_table
+        in
 
-      List.iter (fun (k, v) -> Printf.printf "Type: %s, Count: %d\n" k v) sorted_results;
-      Printf.printf "Read %d lines\n" (List.length csv_list);
+        Csv.iter ~f:(fun row ->
+            batch := row :: !batch;
+            incr lines_read;
+
+            if !lines_read mod batch_size = 0 then (
+                let results = Parmap.parmap ~ncores:4 process_batch (Parmap.L [!batch]) in
+                all_results := results @ !all_results;
+                batch := [];
+            )
+        ) csv;
+
+        (* Process the last batch if it's not empty *)
+        if !batch <> [] then (
+            let results = Parmap.parmap ~ncores:4 process_batch (Parmap.L [!batch]) in
+            all_results := results @ !all_results;
+        );
+
+        let merged_results = merge_tables !all_results in
+
+        let sorted_results = 
+            Hashtbl.to_seq merged_results 
+            |> List.of_seq
+            |> List.sort (fun (_, v1) (_, v2) -> v2 - v1) 
+        in
+
+        List.iter (fun (k, v) -> Printf.printf "Type: %s, Count: %d\n" k v) sorted_results;
+        Printf.printf "Read %d lines\n" !lines_read;
+
+    
